18304431c5b785dbe6c95b3c0feb407207c0e67c
zakdave <74794944+zakdave@users.noreply.github.com>
2020-12-25 17:38:00 -0500

    printquote
    print quote updates

--- js/script.js
+++ js/script.js
@@ -14,64 +14,65 @@ project 1 - A Random Quote Generator
 
 const quotes = [
 {quote: 'It has become appallingly obvious that our technology has exceeded our humanity.',
-name: 'Albert Einstein',
+source: 'Albert Einstein',
 citation: 'The Ultimate Quotable Einstein',
 year: '',
 },
 
 {quote: 'R2-D2, you know better than to trust a strange computer.',
-name: 'C-3PO',
+source: 'C-3PO',
 citation: 'Episode V: The Empire Strikes Back',
 year: '1980',
 },
 
 {quote: "Every generation's breakthroughs are proven false by the next generation's technology",
-name: 'Dan Brown',
+source: 'Dan Brown',
 citation: 'The Lost Symbol',
 year: '2009',
 },
 
 {quote: "Most of the time spent wrestling with technologies that don't quite work yet is just not worth it for end users, however much fun it is for nerds.",
-name: 'Douglas Adams',
+source: 'Douglas Adams',
 citation: '',
 year: '',
 },
 
 {quote: 'I would trade all of my technology for an afternoon with Socrates.',
-name: 'Steve Jobs',
+source: 'Steve Jobs',
 citation: 'Newsweek',
 year: '2001',
 },
 
 {quote: 'Java is to JavaScript what car is to carpet.',
-name: 'Chris Heilmann',
+source: 'Chris Heilmann',
 citation: '',
 year: '',
 },
 
 {quote: 'The real danger is not that computers will begin to think like men, but that men will begin to think like computers.',
-name: 'Sydney J. Harris',
+source: 'Sydney J. Harris',
 citation: '',
 year: '1976',
 },
 
 {quote: "It's still magic even if you know how it's done.",
-name: 'Terry Pratchett',
+source: 'Terry Pratchett',
 citation: 'A Hat Full of Sky',
 year: '2004',
 },
 
 {quote: 'A CD. How quaint. We have these in museums.',
-name: 'Eoin Colfer',
+source: 'Eoin Colfer',
 citation: 'The Eternity Code',
 year: '2003',
 },
 
 {quote: 'Computers are useless. They can only give you answers.',
-name: 'Pablo Picasso',
+source: 'Pablo Picasso',
 citation: '',
 year: '',
-}];
+}
+];
 
 
 /***
@@ -79,9 +80,9 @@ year: '',
  * Create function that returns a random object within
  * quotes array using Math.random
 ***/
-const getRandomQuote = function() {
+const getRandomQuote = () => {
   let random = Math.floor(Math.random() * quotes.length)
-  return quotes[random];
+  return quotes[random]; // returns entire quote object
 };
 
 
@@ -91,9 +92,14 @@ const getRandomQuote = function() {
  * selected through the use of conditional statements
 ***/
 const printQuote = () => {
-  return getRandomQuote().quote;
+  let html = '';
+  let randQuoteObject = getRandomQuote(); // ensures same object is used
+  html += `<p class="quote">${randomQuoteObject.quote}</p>`; 
+  if (randQuoteObject.source !== ''){
+    html += `<p class="quote">${randomQuoteObject.source} `;
+  }
+  return html;
 }
-
 
 /***
  * click event listener for the print quote button
